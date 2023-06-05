55a357428c63790a0b4672c34f91bb34509300e5
Melonours <adrianrumi@hotmail.com>
2023-06-05 14:17:17 +0200

    pipes ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,39 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
-  ngOnInit(): void {
-      this.mySnap = new FaceSnap (
-        'Nirvana',
-        'Mon premier chat',
-        new Date(),
-        0,
-        'https://lemagduchat.ouest-france.fr/images/dossiers/2022-02/assurance-chat-goutiere-171537.jpg',
-      )
+  ngOnInit() {
+    this.faceSnaps = [
+      {
 
-      this.myOtherSnap = new FaceSnap (
-        'Gasoline',
-        'Mon petit chat bibou',
-        new Date(),
-        0,
-        'https://www.josera.fr/media/chat_evaille_de_tortue_3.jpg'
-      )
+        title: 'Nirvana',
+        description: 'Mon premier chat',
+        createdDate: new Date(),
+        snaps: 145,
+        imageUrl: 'https://lemagduchat.ouest-france.fr/images/dossiers/2022-02/assurance-chat-goutiere-171537.jpg',
+        location: 'L-L-N'
+    },
 
-      this.myLastSnap = new FaceSnap (
-        'Nando',
-        'Le petit chiengue',
-        new Date(),
-        0,
-        'https://t1.ea.ltmcdn.com/fr/posts/5/3/0/education_du_golden_retriever_2035_600.jpg'
-      )
+    {
+      title: 'Gasoline',
+      description: 'Mon petit chat bibou',
+      createdDate: new Date(),
+      snaps: 4555,
+      imageUrl: 'https://www.josera.fr/media/chat_evaille_de_tortue_3.jpg',
+      location: 'Ottignies'
+    },
+
+    {
+      title: 'Nando',
+      description: 'Le petit chiengue',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: 'https://t1.ea.ltmcdn.com/fr/posts/5/3/0/education_du_golden_retriever_2035_600.jpg'
+    }
+    ]
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import * as fr from '@angular/common/locales/fr';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent
+    FaceSnapComponent,
+    FaceSnapListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
