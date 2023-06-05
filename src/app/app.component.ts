e2f5db2ba2ed27a22c6ac0a7e9da09a63060098e
Melonours <adrianrumi@hotmail.com>
2023-06-05 14:30:21 +0200

    header ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,39 +7,7 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-
-        title: 'Nirvana',
-        description: 'Mon premier chat',
-        createdDate: new Date(),
-        snaps: 145,
-        imageUrl: 'https://lemagduchat.ouest-france.fr/images/dossiers/2022-02/assurance-chat-goutiere-171537.jpg',
-        location: 'L-L-N'
-    },
-
-    {
-      title: 'Gasoline',
-      description: 'Mon petit chat bibou',
-      createdDate: new Date(),
-      snaps: 4555,
-      imageUrl: 'https://www.josera.fr/media/chat_evaille_de_tortue_3.jpg',
-      location: 'Ottignies'
-    },
-
-    {
-      title: 'Nando',
-      description: 'Le petit chiengue',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'https://t1.ea.ltmcdn.com/fr/posts/5/3/0/education_du_golden_retriever_2035_600.jpg'
-    }
-    ]
-  }
+ 
+  ngOnInit() {}
+   
 }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./face-snap-list.component.scss']
 })
 export class FaceSnapListComponent {
+  faceSnaps!: FaceSnap[];
+ 
+  ngOnInit() {
+    this.faceSnaps = [
+      {
 
+        title: 'Nirvana',
+        description: 'Mon premier chat',
+        createdDate: new Date(),
+        snaps: 145,
+        imageUrl: 'https://lemagduchat.ouest-france.fr/images/dossiers/2022-02/assurance-chat-goutiere-171537.jpg',
+        location: 'L-L-N'
+    },
+
+    {
+      title: 'Gasoline',
+      description: 'Mon petit chat bibou',
+      createdDate: new Date(),
+      snaps: 4555,
+      imageUrl: 'https://www.josera.fr/media/chat_evaille_de_tortue_3.jpg',
+      location: 'Ottignies'
+    },
+
+    {
+      title: 'Nando',
+      description: 'Le petit chiengue',
+      createdDate: new Date(),
+      snaps: 0,
+      imageUrl: 'https://t1.ea.ltmcdn.com/fr/posts/5/3/0/education_du_golden_retriever_2035_600.jpg'
+    }
+    ]
+  }
 }
+
