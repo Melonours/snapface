3ad1ab0f277893c71af06692cfe132e58ebbb2d2
Melonours <adrianrumi@hotmail.com>
2023-06-05 16:55:48 +0200

    routing via buttons ok

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from 'services/face-snaps.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,9 +12,8 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-constructor (private faceSnapsService : FaceSnapsService ){
-
-}
+constructor (private faceSnapsService : FaceSnapsService,
+             private router : Router ){}
 
   ngOnInit() : void{
     this.buttonText= 'Oh Snap!'
@@ -27,5 +27,9 @@ constructor (private faceSnapsService : FaceSnapsService ){
       this.buttonText = 'Oh Snap!';
       this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
     }
+  }
+
+  onViewFaceSnap(){
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
